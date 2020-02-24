a1d7bff991ed3b02417ad9efcab4ae536efc3a7a
esalehi <Ebrahim.Salehi@net-m.de>
2020-02-24 15:37:14 +0100

    fixed the issue with string literal of ip-range

--- terraform/prod/vnet.tf
+++ terraform/prod/vnet.tf
@@ -1,7 +1,7 @@
 variable "vnet_name" {description = "Please insert a name for vnet"}
 
 resource "azurerm_virtual_network" "vnet" {
-  address_space = ['10.10.0.0/16']
+  address_space = ["10.10.0.0/16"]
   location = var.region
   name = var.vnet_name
   resource_group_name = var.rgName
