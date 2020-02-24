9f0f0493f6b673bcaf21e6af5e4d906b93d9097b
esalehi <Ebrahim.Salehi@net-m.de>
2020-02-24 16:12:12 +0100

    added a public subnet

---
+++ terraform/prod/publicSubnet1.tf
@@ -0,0 +1,8 @@
+variable "name" {description = "Please enter a valid name for subnet:pub1"}
+
+resource "azurerm_subnet" "pub1" {
+  address_prefix = "10.10.1.0/24"
+  name = var.name
+  resource_group_name = var.rgName
+  virtual_network_name = azurerm_virtual_network.MyVNet.name
+}
