5d70fc860d4027c02ac3f758df6cbe5753e54db4
esalehi <Ebrahim.Salehi@net-m.de>
2020-02-24 15:52:22 +0100

    Refer to another resource using terraform name of the resource

--- terraform/prod/resourcegroup.tf
+++ terraform/prod/resourcegroup.tf
@@ -1,7 +1,7 @@
 variable "region" {description = "please enter the desired Region"}
 variable "rgName" {description = "Please enter a name for \"Resource Group\""}
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "MyRG" {
   location = var.region
   name = var.rgName
   tags = {
--- terraform/prod/vnet.tf
+++ terraform/prod/vnet.tf
@@ -1,8 +1,8 @@
 variable "vnet_name" {description = "Please insert a name for vnet"}
 
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "MyVNet" {
   address_space = ["10.10.0.0/16"]
   location = var.region
   name = var.vnet_name
-  resource_group_name = var.rgName
+  resource_group_name = azurerm_resource_group.MyRG.name
 }
